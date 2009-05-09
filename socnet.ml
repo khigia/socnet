bedadf28099ed2a262114c0849a46d97caacd3ca
Ludovic Coquelle <lcoquelle@gmail.com>
2009-05-09 15:11:47 +0800

    Gen: adding a wrapper on builder to trace calls.

--- socnet.ml
+++ socnet.ml
@@ -39,10 +39,37 @@ module Gen = struct
 end
 
 
+module TraceBuilder = struct
+  module Make(B : Graph.Builder.INT) = struct
+    module G = B.G
+    let empty = B.empty
+    let copy = B.copy
+    let add_vertex g v =
+      let g = B.add_vertex g v in
+      let _ = Printf.printf "adding vertex: %d\n" (G.V.label v) in
+      g
+    let add_edge g v1 v2 =
+      let g = B.add_edge g v1 v2 in
+      let _ = Printf.printf "adding edge: %d -> %d\n" (G.V.label v1) (G.V.label v2) in
+      g
+    let add_edge_e g e =
+      let g = B.add_edge_e g e in
+      let _ = Printf.printf "adding edge(e) ...\n" in
+      g
+  end
+end
+
 
 let test () =
   let module Gen = Gen.I(Graph.Pack.Digraph) in
   let g = Gen.graph ~vn:26 () in
   Graph.Pack.Digraph.display_with_gv g
 
-let _ = test ()
+let test2 () =
+  let module RealBuilder = Graph.Builder.I(Graph.Pack.Digraph) in
+  let module WrappedBuilder = TraceBuilder.Make(RealBuilder) in
+  let module Gen = Gen.Make(WrappedBuilder) in
+  let g = Gen.graph ~vn:26 () in
+  Graph.Pack.Digraph.display_with_gv g
+
+let _ = test2 ()
