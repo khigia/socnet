93615188de52e10c1348d58ad9878ca47a130c65
Ludovic Coquelle <lcoquelle@gmail.com>
2009-05-28 01:48:16 +0800

    Added a command line tools.

--- socnet.ml
+++ socnet.ml
@@ -2,6 +2,7 @@ let fold_for i0 i1 f =
   let rec loop i v = if i >= i1 then v else loop (i + 1) (f v i) in
   loop i0
 
+(* wrap Graph.Builder to handle a state *)
 module type Builder = sig
   module G : Graph.Sig.G
   type st
@@ -12,7 +13,7 @@ module type Builder = sig
   val add_edge_e : st -> G.t -> G.E.t -> st * G.t
 end
 
-(* convert Graph.Builder to Builder *)
+(* convert Graph.Builder to Builder with empty state *)
 module EmptyBuilderMake(B : Graph.Builder.S) = struct
   module G = B.G
   type st = int
@@ -24,20 +25,46 @@ module EmptyBuilderMake(B : Graph.Builder.S) = struct
 end
 
 
+(* generate random graph by adding node and propagating edges *)
 module Gen = struct
   
+  module IntSet = Set.Make(struct type t = int let compare = compare end)
+
   module Make(B : Builder with type G.V.label = int) = struct
 
-    let _propagate_edges b g v anchor =
-      let b, g = B.add_edge b g v anchor in 
-      let attach_to target (b,g) =
-        if (Random.int 100) >= 50
+    let fwd_p = 0.25
+    let bck_p = 0.35
+
+    let mean = (1. /. (1. -. fwd_p))
+
+    let rec _propagate_edges seen deg b g v anchor =
+      let odeg = B.G.out_degree g anchor in
+      let ideg = B.G.in_degree g anchor in
+      let deg = odeg + ideg in
+      let p = 1000. *. mean /. (float_of_int deg) in
+      let r = bck_p *. p in
+      let ip = int_of_float p in
+      let ir = int_of_float r in
+      let attach_to prob target (s, b, g) =
+        if v == target
         then
-          B.add_edge b g v target
+          s, b, g
         else
-          b,g
+          if IntSet.mem (B.G.V.label target) s
+          then
+            s, b, g
+          else
+            if (Random.int 1000) < prob
+            then
+              let b, g = B.add_edge b g v target in
+              let s = IntSet.add (B.G.V.label target) s in
+              _propagate_edges s deg b g v target
+            else
+              s, b, g
       in
-      B.G.fold_succ attach_to g anchor (b,g)
+      let seen, b, g = B.G.fold_succ (attach_to ip) g anchor (seen, b, g) in
+      let seen, b, g = B.G.fold_pred (attach_to ir) g anchor (seen, b, g) in
+      seen, b, g
 
     let graph ~vn bst =
       let b, g = B.empty bst in
@@ -47,7 +74,9 @@ module Gen = struct
         let v = a.(i) in
         let b, g = B.add_vertex b g v in
         let anchor = a.(Random.int i) in
-        let b, g = _propagate_edges b g v anchor in
+        let b, g = B.add_edge b g v anchor in 
+        let seen = IntSet.singleton (B.G.V.label anchor) in
+        let seen, b, g = _propagate_edges seen 0 b g v anchor in
         b, g
       in
       fold_for 1 vn step (b,g)
@@ -57,27 +86,10 @@ module Gen = struct
 end
 
 
-module TraceBuilder = struct
-  module Make(B : Graph.Builder.INT) = struct
-    module G = B.G
-    let empty = B.empty
-    let copy = B.copy
-    let add_vertex g v =
-      let g = B.add_vertex g v in
-      let _ = Printf.printf "adding vertex: %d\n" (G.V.label v) in
-      g
-    let add_edge g v1 v2 =
-      let g = B.add_edge g v1 v2 in
-      let _ = Printf.printf "adding edge: %d -> %d\n" (G.V.label v1) (G.V.label v2) in
-      g
-    let add_edge_e g e =
-      let g = B.add_edge_e g e in
-      let _ = Printf.printf "adding edge(e) ...\n" in
-      g
-  end
-end
-
-
+(* builder calling ubigraph through xml rpc: duplicate graph in Graph and Ubigraph.
+simple impl: the graph node do not contains the ubigraph ref but we keep track
+through hashtbl
+*)
 module UbigraphBuilder = struct
   type ubi_t = <
     change_edge_style : int32 -> int32 -> int32;
@@ -135,7 +147,7 @@ module UbigraphBuilder = struct
     let add_edge s g v1 v2 =
       let uv1 = Hashtbl.find s.gtou (B.G.V.label v1) in
       let uv2 = Hashtbl.find s.gtou (B.G.V.label v2) in
-      let e = s.u#new_edge uv1 uv2 in
+      let _e = s.u#new_edge uv1 uv2 in
       s, B.add_edge g v1 v2
 
     let add_edge_e s g e = s, B.add_edge_e g e
@@ -143,25 +155,38 @@ module UbigraphBuilder = struct
 end
 
 
-let test () =
-  let module GBuilder = Graph.Builder.I(Graph.Pack.Digraph) in
-  let module TBuilder = TraceBuilder.Make(GBuilder) in
-  let module SBuilder = EmptyBuilderMake(TBuilder) in
-  let module Gen = Gen.Make(SBuilder) in
-  let b, g = Gen.graph ~vn:26 0 in
-  Graph.Pack.Digraph.display_with_gv g
+let _ =
+  try
+    let n = ref 12 in
+    let gv = ref false in
+    let ubi = ref false in
+    let _ = Arg.parse
+      [
+        ("-n", Arg.Int ((:=) n), "number of nodes");
+        ("-gv", Arg.Set gv, "display with gv");
+        ("-ubi", Arg.Set ubi, "display with ubigraph (localhost:20738)");
+      ]
+      ignore
+      ""
+    in
+    let module GBuilder = Graph.Builder.I(Graph.Pack.Digraph) in
+    let module RBuilder = EmptyBuilderMake(GBuilder) in
+    let module RGen = Gen.Make(RBuilder) in
+    let module UBuilder = UbigraphBuilder.Make(GBuilder) in
+    let module UGen = Gen.Make(UBuilder) in
+    let g = if !ubi
+    then
+      let s = UbigraphBuilder.make_state "http://localhost:20738/RPC2" !n in
+      let b, g = UGen.graph ~vn:!n s in
+      g
+    else
+      let s = 1 in
+      let b, g = RGen.graph ~vn:!n s in
+      g
+    in
+    if !gv
+    then
+      Graph.Pack.Digraph.display_with_gv g
 
-let test2 () =
-  let n = 86 in
-  let module GBuilder = Graph.Builder.I(Graph.Pack.Digraph) in
-  let module TBuilder = TraceBuilder.Make(GBuilder) in
-  let module SBuilder = UbigraphBuilder.Make(TBuilder) in
-  let module Gen = Gen.Make(SBuilder) in
-  let s = UbigraphBuilder.make_state "http://localhost:20738/RPC2" n in
-  let b, g = Gen.graph ~vn:n s in
-  Graph.Pack.Digraph.display_with_gv g
-
-let _ = 
-  let _ = test () in
-  let _ = test2 () in
-  ()
+  with
+    exn -> Printf.printf "ERROR:%s\n" (Printexc.to_string exn)
